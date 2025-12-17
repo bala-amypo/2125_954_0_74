164c204390bb93ea93ac1f4ff2c696397532a2e2
bala-amypo <bala@amypo.in>
2025-12-17 06:37:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.*;
+
+import org.springframework.stereotype.service;
+import com.example.demo.entity.Studententity;
+
+@service
+public class Studentservice{
+    private Map<Integer,Studententity> details=new HashMap<>();
+
+    public Studententity
+}
